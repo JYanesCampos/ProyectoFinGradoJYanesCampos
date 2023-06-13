bf957a1f27cba2ac922f60fcc44423052695dabd
Joaquin Yanes Campos <93703559+JYanesCampos@users.noreply.github.com>
2023-06-13 20:11:07 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,11 +1,19 @@
 <?php
 
+// class Config {
+//     //BASE DE DATOS
+//     const BD_HOST = "localhost";
+//     const BD_NAME = "casa_vacaciones";
+//     const BD_USER = "root";
+//     const PASSWORD = "";
+// }
+
 class Config {
     //BASE DE DATOS
-    const BD_HOST = "localhost";
-    const BD_NAME = "casa_vacaciones";
+    const BD_HOST = "containers-us-west-99.railway.app";
+    const BD_NAME = "railway";
     const BD_USER = "root";
-    const PASSWORD = "";
+    const PASSWORD = "Xe4lFOebXhybN30lcbRV";
 }
 
 ?>
